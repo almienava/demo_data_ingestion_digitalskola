d092da745870fcaa5465ab38562fd134e1f63e9f
almienava <haldad.almi@gmail.com>
2022-06-12 11:13:34 +0700

    Update data_ingestion_products.py

--- data_ingestion_products.py
+++ data_ingestion_products.py
@@ -7,11 +7,11 @@ SQL_HOST = os.environ.get('OLTP_HOST')
 SQL_PORT = os.environ.get('OLTP_PORT')
 SQL_DB = os.environ.get('OLTP_DATABASE')
 
-conn = psycopg2.connect(host='digitalskola7-do-user-7592846-0.b.db.ondigitalocean.com', dbname='deadpool', user='doadmin', password='AVNS_TDrSDUoeoSNF5rVVSLx', port='25061')
+conn = psycopg2.connect(host=SQL_HOST, dbname=SQL_DB, user=SQL_USER, password=SQL_PASS, port=SQL_PORT)
 cur = conn.cursor()
-csv_file = open('haldad_products.csv','w')
-sql = 'COPY(select * from products) TO stdout with csv header'
+sql = 'COPY (select * from products) TO STDOUT WITH CSV HEADER'
+csv_file = open('syarif_products.csv','w')
 cur.copy_expert(sql,csv_file)
 csv_file.close()
-cur.close()
-conn.close()
+cur.close
+conn.close
